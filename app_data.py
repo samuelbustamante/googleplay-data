52c772ae5feda2383293b260567de91d7ba45f96
root <root@hip1Android-Develop.(none)>
2012-11-14 23:28:19 +0000

    Add properties

--- app_data.py
+++ app_data.py
@@ -14,26 +14,75 @@ class AppData(object):
         return self.data
 
     def set_data(self):
-        self.set_appDev()
-        self.set_appDesc()
         self.set_appName()
-        self.set_appRating()
+        self.set_appImage()
+        self.set_appAuthorName()
+        self.set_appAuthorUrl()
+        self.set_appDescription()
+        self.set_appVersion()
+        self.set_appPrice()
+        #self.set_appCategory()
+        self.set_appPublished()
+        self.set_appDownloads()
+        self.set_appSize()
+        self.set_appContentRating()
+        self.set_appScreenshots()
 
     def set_appId(self, appId):
         self.data['appId'] = appId
 
-    def set_appDev(self):
-        appDev = self.soup('a', {'class': 'doc-header-link'})[0]
-        self.data['appDev'] = appDev.contents[0]
-
-    def set_appDesc(self):
-        appDesc = self.soup('div', {'itemprop': 'description'})[0]
-        self.data['appDesc'] = ' '.join(appDesc.findAll(text=True))
-
     def set_appName(self):
-        appName = self.soup('h1', {'class': 'doc-banner-title'})[0]
-        self.data['appName'] = appName.contents[0]
+        appName = self.soup('span', {'itemprop': 'name'})[0]
+        self.data['appName'] = appName['content']
+
+    def set_appImage(self):
+        appImage = self.soup('span', {'itemprop': 'image'})[0]
+        self.data['appImage'] = appImage['content']
+
+    def set_appAuthorName(self):
+        appAuthorName = self.soup('span', {'itemprop': 'name'})[1]
+        self.data['appAuthorName'] = appAuthorName['content']
+
+    def set_appAuthorUrl(self):
+        appAuthorUrl = self.soup('span', {'itemprop': 'url'})[0]
+        self.data['appAuthorUrl'] = appAuthorUrl['content']
+
+    def set_appDescription(self):
+        appDescription = self.soup('div', {'itemprop': 'description'})[0]
+        self.data['appDescription'] = ' '.join(appDescription.findAll(text=True))
 
     def set_appRating(self):
         appRating = self.soup('span', {'itemprop': 'ratingCount'})[0]
         self.data['appRating'] = appRating.contents[0]
+
+    def set_appVersion(self):
+        appVersion = self.soup('dd', {'itemprop': 'softwareVersion'})[0]
+        self.data['appVersion'] = appVersion.contents[0]
+
+    def set_appPrice(self):
+        appPrice = self.soup('span', {'itemprop': 'price'})[0]
+        self.data['appPrice'] = appPrice['content']
+
+    def set_appCategory(self):
+        appCategory = self.soup('a', {'href': '/store/apps/category/*'})[0]
+        self.data['appCategory'] = appCategory.contents[0]
+
+    def set_appPublished(self):
+        appPublished = self.soup('time', {'itemprop': 'datePublished'})[0]
+        self.data['appPublished'] = appPublished.contents[0]
+
+    def set_appDownloads(self):
+        appDownloads = self.soup('dd', {'itemprop': 'numDownloads'})[0]
+        self.data['appDownloads'] = appDownloads.contents[0]
+
+    def set_appSize(self):
+        appSize = self.soup('dd', {'itemprop': 'fileSize'})[0]
+        self.data['appSize'] = appSize.contents[0]
+
+    def set_appContentRating(self):
+        appContentRating = self.soup('dd', {'itemprop': 'contentRating'})[0]
+        self.data['appContentRating'] = appContentRating.contents[0]
+
+    def set_appScreenshots(self):
+        appScreenshots = self.soup('img', {'itemprop': 'screenshot'})
+        self.data['screenshots'] = [screenshot['src'] for screenshot in appScreenshots]
